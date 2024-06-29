51e9ada977f860598e1944c3c34290d80a28fce3
MRIGANKA GHOSH <60334844+mrigankaghosh9@users.noreply.github.com>
2024-06-29 14:19:37 +0530

    Update 1251-average-selling-price.sql

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+# 1> Write your MySQL query statement below
 
 select p.product_id,
 ifnull(round(sum(p.price * u.units)/ sum(u.units), 2),0) as average_price
@@ -7,4 +7,14 @@ left join UnitsSold u
 on p.product_id = u.product_id
 and u.purchase_date >= p.start_date 
 and u.purchase_date <= p.end_date
+group by p.product_id
+
+# 2> Write your MySQL query statement below
+
+select p.product_id,
+ifnull(round(sum(p.price * u.units)/ sum(u.units), 2),0) as average_price
+from prices p
+left join UnitsSold u
+on p.product_id = u.product_id
+and u.purchase_date between p.start_date and p.end_date
 group by p.product_id
