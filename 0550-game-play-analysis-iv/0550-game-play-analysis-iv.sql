2ad34f3a39b8bd67622f440c056871bfd53443b4
MRIGANKA GHOSH <60334844+mrigankaghosh9@users.noreply.github.com>
2024-08-25 00:26:04 +0530

    Update 0550-game-play-analysis-iv.sql

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -8,3 +8,25 @@ where (player_id, date_sub(event_date, interval 1 day)) in (
     from activity
     group by player_id
 )
+
+# another sol
+WITH FirstLogin AS (
+    SELECT player_id, MIN(event_date) AS first_login_date
+    FROM Activity
+    GROUP BY player_id
+),
+ConsecutiveLogin AS (
+    SELECT f.player_id
+    FROM FirstLogin f
+    JOIN Activity a
+    ON f.player_id = a.player_id
+    AND DATE_ADD(f.first_login_date, INTERVAL 1 DAY) = a.event_date
+)
+SELECT 
+    ROUND(COUNT(DISTINCT c.player_id) / COUNT(DISTINCT f.player_id), 2) AS fraction
+FROM 
+    FirstLogin f
+LEFT JOIN 
+    ConsecutiveLogin c
+ON 
+    f.player_id = c.player_id;
