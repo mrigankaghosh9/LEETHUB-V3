9e54f07aaf201a07df88345dbbd9686f4597eff1
MRIGANKA GHOSH <60334844+mrigankaghosh9@users.noreply.github.com>
2024-09-08 13:56:05 +0530

    Update 0610-triangle-judgement.sql

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -2,3 +2,12 @@
  select x,y,z,
       if((x+y>z) and (y+z>x) and (z+x>y), 'Yes', 'No') as triangle
  from triangle 
+
+
+# another solution
+SELECT x, y, z,
+       CASE
+           WHEN (x + y > z) AND (x + z > y) AND (y + z > x) THEN 'Yes'
+           ELSE 'No'
+       END AS triangle
+FROM Triangle;
